fea418bb9bf65379d7254f635d6056d053da54d4
Alex Demskie <alex.demskie@gmail.com>
2019-08-10 13:59:14 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { ComputeShader } from "./computeShader";
 import { RenderTarget } from "./renderTarget";
 import { packBooleans, unpackBooleans } from "./vectorBoolArray";
-import { packInt16, unpackInt16 } from "./vectorInt16";
+import { packInt16, unpackInt16, MIN_INT16, MAX_INT16 } from "./vectorInt16";
 
 module.exports = {
   ComputeShader,
@@ -9,10 +9,12 @@ module.exports = {
   packBooleans,
   unpackBooleans,
   packInt16,
-  unpackInt16
+  unpackInt16,
+  MIN_INT16,
+  MAX_INT16
 };
 
 export { ComputeShader } from "./computeShader";
 export { RenderTarget } from "./renderTarget";
 export { packBooleans, unpackBooleans } from "./vectorBoolArray";
-export { packInt16, unpackInt16 } from "./vectorInt16";
+export { packInt16, unpackInt16, MIN_INT16, MAX_INT16 } from "./vectorInt16";
