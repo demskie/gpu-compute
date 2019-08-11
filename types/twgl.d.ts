c736820bb759a51d372f4c4b35ed37823139487f
Alex Demskie <alex.demskie@gmail.com>
2019-08-10 18:46:10 -0700

    0.0.10

--- types/twgl.d.ts
+++ types/twgl.d.ts
@@ -1,11 +1,14 @@
 /// <reference lib="es2017.sharedmemory" />
 
 declare global {
-  // type SharedArrayBuffer = any; // the above reference lib resolves this declaration
-  type WebGLTransformFeedback = any;
-  type WebGLVertexArrayObject = any;
-  type WebGL2RenderingContext = any;
-  type WebGLSampler = any;
+  // <reference lib="es2017.sharedmemory" resolves this declaration
+  // type SharedArrayBuffer = any;
+  //
+  // devDependency: "@types/webgl2" resolves the following declarations
+  // type WebGLTransformFeedback = any;
+  // type WebGLVertexArrayObject = any;
+  // type WebGL2RenderingContext = any;
+  // type WebGLSampler = any;
 }
 
 // If your module exports nothing you need this line
