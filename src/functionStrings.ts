e99c1f92017ec909136449a21f378058f69d9ada
demskie <alex.demskie@gmail.com>
2020-03-07 00:20:36 -0600

    implement texcoord functions

--- src/functionStrings.ts
+++ src/functionStrings.ts
@@ -25,12 +25,12 @@ float floatGreaterThan(float f1, float f2) {
 
 export const floatLessThanOrEqual = `
 float floatLessThanOrEqual(float f1, float f2) {
-  return 1.0 - floatGreaterThan(f1, f2);
+  return 1.0 - max(sign(f1 - f2), 0.0);
 }`.trim();
 
 export const floatGreaterThanOrEqual = `
 float floatGreaterThanOrEqual(float f1, float f2) {
-  return 1.0 - floatLessThan(f1, f2);
+  return 1.0 - max(sign(f2 - f1), 0.0); 
 }`.trim();
 
 export const vec2ToInt16 = `
@@ -58,21 +58,21 @@ vec2 uint16ToVec2(float f) {
 export const unpackBooleans = `
 void unpackBooleans(float f, inout bool arr[8]) {
   f = floor(f * 255.0);
-  arr[0] = bool(int(floatGreaterThanOrEqual(f, 128.0)));
-  f -= floatGreaterThanOrEqual(f, 128.0) * 128.0;
-  arr[1] = bool(int(floatGreaterThanOrEqual(f, 64.0)));
-  f -= floatGreaterThanOrEqual(f, 64.0) * 64.0;
-  arr[2] = bool(int(floatGreaterThanOrEqual(f, 32.0)));
-  f -= floatGreaterThanOrEqual(f, 32.0) * 32.0;
-  arr[3] = bool(int(floatGreaterThanOrEqual(f, 16.0)));
-  f -= floatGreaterThanOrEqual(f, 16.0) * 16.0;
-  arr[4] = bool(int(floatGreaterThanOrEqual(f, 8.0)));
-  f -= floatGreaterThanOrEqual(f, 8.0) * 8.0;
-  arr[5] = bool(int(floatGreaterThanOrEqual(f, 4.0)));
-  f -= floatGreaterThanOrEqual(f, 4.0) * 4.0;
-  arr[6] = bool(int(floatGreaterThanOrEqual(f, 2.0)));
-  f -= floatGreaterThanOrEqual(f, 2.0) * 2.0;
-  arr[7] = bool(int(floatGreaterThanOrEqual(f, 1.0)));
+  arr[0] = bool(int(1.0 - max(sign(128.0 - f), 0.0)));  
+  f -= (1.0 - max(sign(128.0 - f), 0.0)) * 128.0;
+  arr[1] = bool(int(1.0 - max(sign(64.0 - f), 0.0)));
+  f -= (1.0 - max(sign(64.0 - f), 0.0)) * 64.0;
+  arr[2] = bool(int(1.0 - max(sign(32.0 - f), 0.0)));
+  f -= (1.0 - max(sign(32.0 - f), 0.0)) * 32.0;
+  arr[3] = bool(int(1.0 - max(sign(16.0 - f), 0.0)));
+  f -= (1.0 - max(sign(16.0 - f), 0.0)) * 16.0;
+  arr[4] = bool(int(1.0 - max(sign(8.0 - f), 0.0)));
+  f -= (1.0 - max(sign(8.0 - f), 0.0)) * 8.0;
+  arr[5] = bool(int(1.0 - max(sign(4.0 - f), 0.0)));
+  f -= (1.0 - max(sign(4.0 - f), 0.0)) * 4.0;
+  arr[6] = bool(int(1.0 - max(sign(2.0 - f), 0.0)));
+  f -= (1.0 - max(sign(2.0 - f), 0.0)) * 2.0;
+  arr[7] = bool(int(1.0 - max(sign(1.0 - f), 0.0)));
 }`.trim();
 
 export const packBooleans = `
@@ -85,6 +85,90 @@ float packBooleans(bool arr[8]) {
   f += float(int(arr[5])) * 4.0;
   f += float(int(arr[6])) * 2.0;
   return (f + float(int(arr[7]))) / 255.0;
+}`.trim();
+
+export const texcoord = `
+struct texcoord { float x, y, w; };
+`.trim();
+
+export const add = `
+texcoord add(texcoord t, float f) {
+  t.y = t.y + floor(f / t.w) + floor((t.x + mod(f, t.w)) / t.w);
+  t.x = mod(t.x + f, t.w);
+  return t;
+}`.trim();
+
+export const subtract = `
+texcoord subtract(texcoord t, float f) {
+  t.y = t.y - floor(f / t.w) + floor((t.x - mod(f, t.w)) / t.w);
+  t.x = mod(t.x - f, t.w);
+  return t;
+}`.trim();
+
+export const oneSixteenthTexcoord = `
+texcoord oneSixteenthTexcoord(texcoord t) {
+  t.x = floor(t.x / 2.0) + floor(t.w / 2.0) * mod(t.y, 2.0);
+  t.y = floor(t.y / 2.0) + floor(t.x / t.w);
+  t.x = mod(t.x, t.w);
+  t.x = floor(t.x / 2.0) + floor(t.w / 2.0) * mod(t.y, 2.0);
+  t.y = floor(t.y / 2.0) + floor(t.x / t.w);
+  t.x = mod(t.x, t.w);
+  t.x = floor(t.x / 2.0) + floor(t.w / 2.0) * mod(t.y, 2.0);
+  t.y = floor(t.y / 2.0) + floor(t.x / t.w);
+  t.x = mod(t.x, t.w);
+  return t;
+}`.trim();
+
+export const oneFourthTexcoord = `
+texcoord oneFourthTexcoord(texcoord t) {
+  t.x = floor(t.x / 2.0) + floor(t.w / 2.0) * mod(t.y, 2.0);
+  t.y = floor(t.y / 2.0) + floor(t.x / t.w);
+  t.x = mod(t.x, t.w);
+  t.x = floor(t.x / 2.0) + floor(t.w / 2.0) * mod(t.y, 2.0);
+  t.y = floor(t.y / 2.0) + floor(t.x / t.w);
+  t.x = mod(t.x, t.w);
+  return t;
+}`.trim();
+
+export const oneHalfTexcoord = `
+texcoord oneHalfTexcoord(texcoord t) {
+  t.x = floor(t.x / 2.0) + floor(t.w / 2.0) * mod(t.y, 2.0);
+  t.y = floor(t.y / 2.0) + floor(t.x / t.w);
+  t.x = mod(t.x, t.w);
+  return t;
+}`.trim();
+
+export const doubleTexcoord = `
+texcoord doubleTexcoord(texcoord t) {
+  t.x = floor(t.x * 2.0);
+  t.y = floor(t.y * 2.0) + floor(t.x / t.w);
+  t.x = mod(t.x, t.w);
+  return t;
+}`.trim();
+
+export const quadrupleTexcoord = `
+texcoord quadrupleTexcoord(texcoord t) {
+  t.x = floor(t.x * 2.0);
+  t.y = floor(t.y * 2.0) + floor(t.x / t.w);
+  t.x = mod(t.x, t.w);
+  t.x = floor(t.x * 2.0);
+  t.y = floor(t.y * 2.0) + floor(t.x / t.w);
+  t.x = mod(t.x, t.w);
+  return t;
+}`.trim();
+
+export const sexdecupleTexcoord = `
+texcoord sexdecupleTexcoord(texcoord t) {
+  t.x = floor(t.x * 2.0);
+  t.y = floor(t.y * 2.0) + floor(t.x / t.w);
+  t.x = mod(t.x, t.w);
+  t.x = floor(t.x * 2.0);
+  t.y = floor(t.y * 2.0) + floor(t.x / t.w);
+  t.x = mod(t.x, t.w);
+  t.x = floor(t.x * 2.0);
+  t.y = floor(t.y * 2.0) + floor(t.x / t.w);
+  t.x = mod(t.x, t.w);
+  return t;
 }`.trim();
 
 export const functionStrings = {
@@ -100,5 +184,12 @@ export const functionStrings = {
   vec2ToUint16,
   uint16ToVec2,
   unpackBooleans,
-  packBooleans
+  packBooleans,
+  texcoord,
+  oneSixteenthTexcoord,
+  oneFourthTexcoord,
+  oneHalfTexcoord,
+  doubleTexcoord,
+  quadrupleTexcoord,
+  sexdecupleTexcoord
 };
