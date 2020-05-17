50e28d106acd354d9828e31b9e53bace2a0f06f6
demskie <alex.demskie@gmail.com>
2020-05-17 06:55:50 -0500

    Updates

---
+++ precache-manifest.2ed6b35e7efbce80fee7078490373387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88110c02aeb2569f53afad21d6881e41",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b44e848e6e590594020",
+    "url": "./static/css/main.789974f3.chunk.css"
+  },
+  {
+    "revision": "2b0f1bdf616e522f2185",
+    "url": "./static/js/2.9747513c.chunk.js"
+  },
+  {
+    "revision": "24cbbbb26d3143f62c679736740b1562",
+    "url": "./static/js/2.9747513c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b44e848e6e590594020",
+    "url": "./static/js/main.f0ebf884.chunk.js"
+  },
+  {
+    "revision": "87dc95f401a5e6f4f6f0",
+    "url": "./static/js/runtime-main.52fae5b8.js"
+  }
+]);
